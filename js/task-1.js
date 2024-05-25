8911fb7b7dd9b8a14b0df7ab77aafddf525a8517
Sergey Melnichenko <159032191+SerhiiMelnichenko@users.noreply.github.com>
2024-05-25 12:31:02 +0300

    c0m

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,87 @@
 
+//  Використовуючи if...else напиши код, який буде запитувати у користувача "Яка офіційна назва JavaScript?".
+//    Якщо користувач вводить "ECMAScript", то показати за допомогою alert("Вірно!")
+//    У інших випадках показати "Не знаєте? ECMAScript!"
 
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//         let totalPrice = quantity * pricePerDroid
-//         return totalPrice > customerCredits ? `"Insufficient funds!"` : `"You ordered ${quantity} droids worth ${totalPrice} credits!"`}
+// const message = prompt("Яка офіційна назва JavaScript?")
+// console.log(message)
+// if (message?.toLowerCase() === "ECMAScript".toLowerCase()) {
+//     alert("Вірно!")
+// } else {
+//     alert("Не знаєте? ECMAScript!")
+//     }
+//===============================================================================================
+//  Напишіть програму, яка отримає від користувача число(кількість хвилин)
+// і виведе у консоль рядок у форматі годин та хвилин
+// 70 === 01: 10
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// const globalMinutes = 90
+// const minutesToHours = String(Math.floor(globalMinutes / 60).padStart(2, 0))
+// //console.log(minutesToHours)
+// const minuterAfterHour = String(globalMinutes % 60).padStart(2, 0)
+// //console.log(minuterAfterHour)
+// console.log(`${minutesToHours}: ${minuterAfterHour}`)
+//===============================================================================================
+//  Напишіть цикл, який виводить у консоль числа від max до min у порядку зменшення.
+// У консоль виведіть усі парні числа від max  до min
+
+// const max = 50
+// const min = 23
+// for (let i = max; i >= min; i -= 1) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+//==================================================================================================
+// За допомогою цикла for додайте всі парні числа від min до max//!
+// const max = 50
+// const min = 0
+// let sum = 0
+// for (let i = min; i <= max; i++) {
+//     if (i % 2 === 0) {sum+=1
+// }
+// }
+// console.log(i)
+//=====================================================================================================
+// При завантаженні сторінки користувачу пропонується у prompt ввести число.
+//     Те, що вводить користувач, додається до значення змінної total.
+// Операція вводу числа триває до тих пір, поки користувач не натисне кнопку Cancel в prompt.
+// Після того, як користувач припинив вводити числа і натиснув на кнопку Cancel,
+//     показувати alert з рядком "Загальна сума введених чисел дорівнює [число]."
+// Робити перевірку, що користувач ввів саме число, а не довільні символи, не потрібно.
+// let number = prompt("Ввести число!")
+// let sum = 0
+// while (number != null) {
+//     sum + - Number(number)
+//     number = prompt("Ввести число!")
+// }
+// alert(`Загальна сума введених чисел дорівнює ${sum}.`)
+//==========================================================================================================
+
+//Напиши код, який буде запитувати логін за допомогою prompt та логувати результат у консоль браузера.
+// Якщо користувач вводить "Адмін", то prompt запитує пароль.Якщо нічого не введено або натиснута клавіша Esc,
+//     вивести рядок "Відмінено"
+//     У іншому випадку вивести рядок "Я вас не знаю!"
+
+// // Пароль перевіряти так:
+// // якщо введено пароль "Я головний", то вивести "Вітаю!",
+// //     у іншому випадку виводити рядок "Невірний пароль!".
+// const login = prompt("Enter login")
+// if (login === "Адмін") {
+//     const password = prompt("Enter password")
+//     if (password === "Я головний") {
+//         alert("Вітаю!")
+        
+//     }else{ alert("Невірний пароль!")}
+// } else if (!login) {
+//     alert("Відмінено")
+// } else {
+//     alert("Я вас не знаю!")
+// }
 
 
-console.log(makeTransaction(5, 3000, 23000)) // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)) // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)) // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)) // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)) // "You ordered 10 droids worth 5000 credits!"
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid
-    if (totalPrice > customerCredits) { return `"Insufficient funds!"` }
-    else { return `"You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!"` }
-}
+
+
+
+
